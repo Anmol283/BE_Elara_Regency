ad4eb288c630d594c0cea6c1dcf5cfcc4f4c1a28
Arsh Singh <arshth134@gmail.com>
2025-04-15 22:10:19 +0530

    empty

--- middlewares/compression.js
+++
@@ -1,6 +0,0 @@
-const compression = require('compression');
-
-// Compression middleware to compress response bodies for performance
-const compress = compression();
-
-module.exports = compress;
--- middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-const morgan = require('morgan');
-
-// Morgan middleware to log HTTP requests in a combined format
-const logger = morgan('dev');
-
-module.exports = logger;
